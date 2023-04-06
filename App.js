22c340098e6eb616e165939105c9985c8347dd84
gogoSpace-tutor <gogoSpace01@gmail.com>
2023-04-06 23:04:05 +0300

    fetching users

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View } from 'react-native';
+import {  View } from 'react-native';
 import { gStyle } from './Style/style';
 import HomepageStack from './navigate';
 import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import * as SplashScreen from 'expo-splash-screen';
 
-
-
 export default function App() {
-  const [userData, setUserData] = useState([]);
-  
-  let [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-  });
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+	const [userData, setUserData] = useState([]);
 
-  useEffect(() => {
-    SplashScreen.preventAutoHideAsync();
-    }, []);
+	let [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+	});
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
 
-  if (!fontsLoaded) {
-    return null;
-  }
- 
-  return (
-    <View style={gStyle.main} onLayout={onLayoutRootView}>
-      
-      <HomepageStack userData={userData} setUserData={setUserData} />
-    </View>
-  );
+	useEffect(() => {
+		SplashScreen.preventAutoHideAsync();
+	}, []);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	return (
+		<View style={gStyle.main} onLayout={onLayoutRootView}>
+			<HomepageStack userData={userData} setUserData={setUserData} />
+		</View>
+	);
 }
--- components/AsyncStorageMethods.js
+++ components/AsyncStorageMethods.js
@@ -2,33 +2,38 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // methods with AsyncStorage
 const storeItemToAsyncStorage = async (whatToStore, arrayToStore) => {
-    try {
-        await AsyncStorage.setItem(whatToStore, JSON.stringify(arrayToStore));
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await AsyncStorage.setItem(whatToStore, JSON.stringify(arrayToStore));
+	} catch (error) {
+		console.log(error);
+	}
 };
 const getItemFromAsyncStorage = async (whatToGet) => {
-    try {
-        const returnedData = await AsyncStorage.getItem(whatToGet);
-        return returnedData != null ? JSON.parse(returnedData) : null;
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const returnedData = await AsyncStorage.getItem(whatToGet);
+		return returnedData != null ? JSON.parse(returnedData) : null;
+	} catch (error) {
+		console.log(error);
+	}
 };
 const mergeItemInAsyncStorage = async (whatToMerge, arrayToMerge) => {
-    try {
-        await AsyncStorage.mergeItem(whatToMerge, JSON.stringify(arrayToMerge));
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await AsyncStorage.mergeItem(whatToMerge, JSON.stringify(arrayToMerge));
+	} catch (error) {
+		console.log(error);
+	}
 };
 const deleteItemFromAsyncStorage = async (whatToDelete) => {
-    try {
-        await AsyncStorage.removeItem(whatToDelete);
-    } catch (error) {
-        console.log(error);
-    }
-    //console.log('Done.')
+	try {
+		await AsyncStorage.removeItem(whatToDelete);
+	} catch (error) {
+		console.log(error);
+	}
+	//console.log('Done.')
 };
-export {storeItemToAsyncStorage, getItemFromAsyncStorage, mergeItemInAsyncStorage, deleteItemFromAsyncStorage};
+export {
+	storeItemToAsyncStorage,
+	getItemFromAsyncStorage,
+	mergeItemInAsyncStorage,
+	deleteItemFromAsyncStorage,
+};
